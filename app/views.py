56555f99de9c7304059eac436ef600b83df3dee6
njuwangchen <wangchenclark0512@gmail.com>
2015-03-14 20:33:15 +0800

    完成v1版本的restful接口

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ __author__ = 'ClarkWong'
 
 from app import db, app
 from flask.ext.restful import reqparse, abort, Api, Resource, fields, marshal_with, marshal
-from flask import jsonify
 from models import User
 
 api = Api(app)
@@ -25,14 +24,35 @@ class UserApi(Resource):
         self.parser.add_argument('description', type=str)
         super(UserApi, self).__init__()
 
+    @marshal_with(user_fields)
     def get(self, user_id):
-        pass
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            return user, 201
+        else:
+            abort(404, message='User {} not found'.format(user_id))
 
     def delete(self, user_id):
-        pass
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return { 'message' : 'Delete User {} succeed'.format(user_id)}, 201
+        else:
+            abort(404, message='User {} not found'.format(user_id))
 
+    @marshal_with(user_fields)
     def put(self, user_id):
-        pass
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            args = self.parser.parse_args()
+            for k,v in args.iteritems():
+                if v!= None:
+                    setattr(user, k, v)
+            db.session.commit()
+            return user, 201
+        else:
+            abort(404, message='User {} not found'.format(user_id))
 
 
 class UserListApi(Resource):
@@ -47,7 +67,10 @@ class UserListApi(Resource):
 
     def get(self):
         userList = User.query.all()
-        return [marshal(user, user_fields) for user in userList]
+        if userList:
+            return [marshal(user, user_fields) for user in userList]
+        else:
+            abort(404, message='No User at all')
 
     @marshal_with(user_fields)
     def post(self):
@@ -61,5 +84,5 @@ class UserListApi(Resource):
         db.session.commit()
         return user, 201
 
-api.add_resource(UserListApi, '/users', endpoint='userList')
-api.add_resource(UserApi, '/user/<user_id>', endpoint='user')
+api.add_resource(UserListApi, '/api/v1/users', endpoint='userList')
+api.add_resource(UserApi, '/api/v1/user/<user_id>', endpoint='user')
