a6c50b86ba49b6651066f5fd6da228d54a2614a0
njuwangchen <wangchenclark0512@gmail.com>
2015-03-14 13:30:53 +0800

    编写了User Model和db migrate部分模块

--- app/models.py
+++ app/models.py
@@ -1,1 +1,21 @@
 __author__ = 'ClarkWong'
+
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    privilege = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.text)
+
+    def __init__(self, name, password, privilege=0, description=''):
+        self.name = name
+        self.password = password
+        self.privilege = privilege
+        self.description = description
+
+
+
+
+
