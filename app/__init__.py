1d171517a719116448c434a7d10cccd5152b15a4
njuwangchen <wangchenclark0512@gmail.com>
2015-03-17 16:54:47 +0800

    添加了对于cross domain的支持

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ __author__ = 'ClarkWong'
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+cor = CORS(app, allow_headers='Content-Type')
 
 from app import views, models
